b73b87ed21014d0e5bb96e856b47137f705dda16
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2024-10-30 10:24:26 +0200

    first commit

--- chapter02/src/App.js
+++ chapter02/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Products from "./Products";
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
 
 function App() {
+  const user = {
+    firstName: "Alexander",
+    lastName: "Badenhorst",
+    imageUrl: "https://picsum.photos/200/300",
+  };
   return (
     <div>
-      <h1>Learn React</h1>
+      <h1>
+        Hello {formatName(user)}!
+        <Products />
+        <br />
+        <img src={user.imageUrl}></img>
+      </h1>
     </div>
   );
 }
