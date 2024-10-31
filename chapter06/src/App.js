c15e05966c1d7d417d92af381b0334b81c4ac316
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2024-10-31 10:11:28 +0200

    first commit

---
+++ chapter06/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Products from "./Products";
+import JumbotronComponent from './JumbotronComponent';
+
+function App() {
+  return (
+    <div>
+      <JumbotronComponent>
+      This is a simple hero unit, a simple jumbotron-style component for
+      calling extra attention to featured content or information.
+      </JumbotronComponent>
+    </div>
+  );
+}
+
+export default App;
